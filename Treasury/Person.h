f9f4c6b618e8719e1c760b64cc5867d152fafb78
Bartosz <777Newton777@gmail.com>
2021-05-23 10:18:38 +0200

    added setters ang getters for person class

--- Treasury/Person.h
+++ Treasury/Person.h
@@ -10,13 +10,73 @@ class Person
 private:
 	std::string name;
 	std::string surname;
-	int age;
+	char age;
 	int allPayments = 0;
 	int allIncomesSettled = 0;
 	TwoWayList<Contract> contracts;
 	void addContract(Contract contr);
 	void delContract(Contract contr);
 public:
+	std::string getName() const noexcept
+	{
+		return name;
+	}
+
+	std::string getSurname() const noexcept
+	{
+		return surname;
+	}
+	
+	char getAge() const noexcept
+	{
+		return age;
+	}
+
+	int getAllPayments() const noexcept
+	{
+		return allPayments;
+	}
+
+	int getAllIncomesSettled() const noexcept
+	{
+		return allIncomesSettled;
+	}
+
+	TwoWayList<Contract> getContracts() const noexcept
+	{
+		return contracts;
+	}
+
+	void setName(std::string nam)
+	{
+		name = nam;
+	}
+
+	void setSurname(std::string surnam)
+	{
+		surname = surnam;
+	}
+
+	void setAge(char ag)
+	{
+		age = ag;
+	}
+
+	void setAllPayments(int number)
+	{
+		allPayments = number;
+	}
+
+	void setAllIncomesSettled(int number)
+	{
+		allIncomesSettled = number;
+	}
+
+	void setContracts(TwoWayList<Contract> contr)
+	{
+		contracts = contr;
+	}
+
 	void addContr(Contract contr); // for testing
 	void delContr(Contract contr); // for testing
 	Person(std::string nam, std::string surnam, int ag);
