0f426f1f15e72971268011c572926a303b7f207c
Marcin Grabysz <01158697@pw.edu.pl>
2021-05-23 18:54:39 +0200

    add payments headers

---
+++ Treasury/Payments.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Person.h"
+
+class Payment
+	// virtual base class for all payments
+{
+public:
+	virtual int calculate(Person& person, Contract& const contract) = 0;
+	/* This function modifies person attributes: allIncomesSettled and allPayments
+	Contract remains unchanged
+	Function returns value of the payment added to person.allPayments (not necessary,
+	but may be helpful) */
+};
+
+class Retirement :public Payment
+	// pension contribution (pol.: sk³adka emerytalna)
+{
+public:
+	virtual int calculate(Person& person, Contract& const contract);
+};
+
+class Pension :public Payment
+	// disablement pension (pol.: sk³adka rentowa)
+{
+public:
+	virtual int calculate(Person& person, Contract& const contract);
+};
+
+class Tax :public Payment
+	// income tax (pol.: podatek dochodowy)
+{
+public:
+	virtual int calculate(Person& person, Contract& const contract);
+};
+
+class Health :public Payment
+	// Health insurance (pol.: sk³adka zdrowotna)
+{
+public:
+	virtual int calculate(Person& person, Contract& const contract);
+};
+
+class Illness :public Payment
+	// Another health insurance (pol.: sk³adka chorobowa, nawet nie wiem jak to przet³umaczyæ)
+{
+public:
+	virtual int calculate(Person& person, Contract& const contract);
+};
