09ddb971005196eca7ca0ee7dba1402225bd1abd
Łukasz Szarejko <mslukaszsz@wp.pl>
2021-05-26 16:56:59 +0200

    Create project for Calculator tests

---
+++ CalculatorTest/CalculatorTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorTest
+{
+	TEST_CLASS(CalculatorTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
