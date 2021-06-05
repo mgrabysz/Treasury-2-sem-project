294d8cfabe8261147e1bfc0440e2de5650c14e91
Łukasz Szarejko <mslukaszsz@wp.pl>
2021-06-05 11:47:21 +0200

    add main to MainApp

--- MainApp/MainApp.cpp
+++ MainApp/MainApp.cpp
@@ -1,17 +1,41 @@
-#include "../Treasury/Treasury.h"
+#include "../Treasury/ConsoleInterface.h"
 #include <iostream>
 
-//int main()
-//{
-//    Person szary("Lukasz", "Szarejko", 20);
-//    Contract* biedaKontrakt = new Contract(zlecenie, 280000);
-//    //std::cout << szary.getName() << std::endl;
-//    //std::cout << biedaKontrakt.value << std::endl;
-//    Treasury treasury;
-//    Person* ptr = new Person("Marcin", "Grabysz", 20);
-//    treasury.addPerson(ptr);
-//    treasury.editPerson(1, biedaKontrakt);
-//    //std::cout << treasury.generateListPayment();
-//    //std::cout << treasury.showPeople();
-//    return 0;
-//}
+int main()
+{
+	ConsoleInterface cInterface;
+	std::cout << "Welcome in Treasury!\nImporting data from json...\n";
+	std::string path = "people.json";
+	cInterface.insertPeopleFromJson(path);
+	std::cout << "Done!";
+	bool end = false;
+	while (!end)
+	{
+		std::cout << "What do you want to do?\n1. Generale list of settlements\n2. Generate settlement for one person\n3. Add person\n4. Delete person\n5. Edit person\n6. Exit";
+		int opt = 0;
+		switch (opt)
+		{
+		case 1:
+			cInterface.exportListSettlementToJson();
+			std::cout << "Export completed!\n";
+		case 2:
+			cInterface.exportGeneratedSettlementPerson();
+			std::cout << "Export completed!\n";
+		case 3:
+			cInterface.addNewPerson();
+			std::cout << "Operation completed\n";
+		case 4:
+			cInterface.deletePerson();
+			std::cout << "Deleted succesfully\n";
+		case 5:
+			cInterface.editPerson();
+			std::cout << "Operation completed\n";
+		case 6:
+			std::cout << "Thank you for using our services.\n";
+			end = true;
+		default:
+			std::cout << "Wrong input!\nTry again\n";
+		}
+	}
+}
+
